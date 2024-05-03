585ab98df509f7321f271bae192cfd61278ee6d2
Scott <mr.scott.mcallister@gmail.com>
2024-05-03 00:22:07 -0300

    add db connection

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/scottmcallister/go-hello
 
 go 1.22.1
+
+require github.com/mattn/go-sqlite3 v1.14.22 // indirect
--- hello.go
+++ hello.go
@@ -1,23 +1,84 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func main() {
-	http.HandleFunc("/", helloHandler)
-	http.ListenAndServe(":8080", nil)
+type App struct {
+	DB *sql.DB
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
+type Grocery struct {
+	ID     int
+	Name   string
+	InCart bool
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	app := &App{DB: db}
+
+	// print hello world
+	fmt.Println("listening on port 8080")
+
+	http.HandleFunc("/", app.index)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func (app *App) index(w http.ResponseWriter, r *http.Request) {
+	// fetch data from groceries table in database
+	rows, err := app.DB.Query("SELECT * FROM groceries")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
 	tmpl, err := template.ParseFiles("index.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	err = tmpl.Execute(w, nil)
+
+	data := rowsToGroceries(rows)
+
+	err = tmpl.Execute(w, data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func rowsToGroceries(rows *sql.Rows) []Grocery {
+	if rows == nil {
+		log.Println("rows is nil")
+		return nil
+	}
+	if err := rows.Err(); err != nil {
+		log.Println(err)
+		return nil
+	}
+	var groceries []Grocery
+	for rows.Next() {
+		var grocery Grocery
+		err := rows.Scan(&grocery.ID, &grocery.Name, &grocery.InCart)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		groceries = append(groceries, grocery)
+	}
+	fmt.Println(groceries)
+	return groceries
 }
