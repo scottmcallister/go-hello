bd870f7f17b0a4af2e2fb3efa8e2f11c15d0308f
Scott <mr.scott.mcallister@gmail.com>
2024-05-02 23:23:16 -0300

    add html page

--- hello.go
+++ hello.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	http.HandleFunc("/", helloHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
