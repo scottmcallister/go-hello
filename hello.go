260ba870fe203046900dd6385a2befcee6ff7a1a
Scott <mr.scott.mcallister@gmail.com>
2024-05-04 23:39:10 -0300

    add post endpoint

--- hello.go
+++ hello.go
@@ -33,18 +33,14 @@ func main() {
 	fmt.Println("listening on port 8080")
 
 	http.HandleFunc("/", app.index)
+	http.HandleFunc("/add", app.add)
 	http.ListenAndServe(":8080", nil)
 
 }
 
 func (app *App) index(w http.ResponseWriter, r *http.Request) {
 	// fetch data from groceries table in database
-	rows, err := app.DB.Query("SELECT * FROM groceries")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
+	data := app.getGroceriesFromDB()
 
 	tmpl, err := template.ParseFiles("index.html")
 	if err != nil {
@@ -52,12 +48,23 @@ func (app *App) index(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data := rowsToGroceries(rows)
-
 	err = tmpl.Execute(w, data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func (app *App) getGroceriesFromDB() []Grocery {
+	rows, err := app.DB.Query("SELECT * FROM groceries")
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	defer rows.Close()
+
+	data := rowsToGroceries(rows)
+
+	return data
 }
 
 func rowsToGroceries(rows *sql.Rows) []Grocery {
@@ -81,4 +88,33 @@ func rowsToGroceries(rows *sql.Rows) []Grocery {
 	}
 	fmt.Println(groceries)
 	return groceries
+}
+
+func (app *App) add(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	name := r.FormValue("name")
+	if name == "" {
+		http.Error(w, "name is required", http.StatusBadRequest)
+		return
+	}
+
+	_, err := app.DB.Exec("INSERT INTO groceries (name, inCart) VALUES (?, ?)", name, false)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	data := app.getGroceriesFromDB()
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
